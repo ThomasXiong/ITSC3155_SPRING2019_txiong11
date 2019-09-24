32765cbca9c7a7a5422ecce96e434a391455c42c
Thomas Xiong <txiong11@uncc.edu>
2019-09-24 16:43:26 +0000

    Ruby 00 Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -6,9 +6,14 @@ attr_accessor :isbn, :price
 
 def initialize (isbn, price)
     
-    @isbn = isbn
-    @price = price 
+raise  ArgumentError if isbn.empty? || price <= 0
+@isbn = isbn
+@price = price 
     
 end
+
+
+
+
 
 end
