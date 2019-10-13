6c6d0635c9b2a6e6ee68a575423e0bd611fd0435
Thomas Xiong <txiong11@uncc.edu>
2019-10-13 19:30:58 +0000

    Complete Create and Show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,6 +4,26 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params.require(:article).permit(:title, :text))
+        
+        @article.save
+        redirect_to @article
+        
+        def show
+    @article = Article.find(params[:id])
+        end 
+
     end
 end
+
+private
+def article_params
+    params.require(:article).permit(:title, :text)
+end 
+
+def show
+    @article = Article.find(params[:id])
+end 
+
+
+
